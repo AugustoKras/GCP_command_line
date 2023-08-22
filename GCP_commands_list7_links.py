1238d4b14d10581e361de71214b79e46b2074912
Augusto Kraszczuk <61908423+AugustoKras@users.noreply.github.com>
2023-08-22 15:02:32 -0300

    Update and rename GCP_commands_list7_links.txt to GCP_commands_list7_links.py

---
+++ GCP_commands_list7_links.py
@@ -0,0 +1,17 @@
+***Cloud IAM: Qwik Start
+
+Lab Skillsboost [https://partner.cloudskillsboost.google/focuses/11544?parent=catalog]
+
+Lab Resolution Youtube [https://www.youtube.com/watch?v=Qly6Z3U74Gg]
+
+***IAM Custom Roles 
+
+Lab Skillsboost [https://partner.cloudskillsboost.google/focuses/11638?parent=catalog]
+
+Lab Resolution Youtube [https://www.youtube.com/watch?v=9X5jWjmT88Y]
+
+***VPC Network Peering
+
+Lab Skillsboost [https://partner.cloudskillsboost.google/focuses/11643?parent=catalog]
+
+Lab Resolution Youtube [https://www.youtube.com/watch?v=VbubgXV0lZI]
